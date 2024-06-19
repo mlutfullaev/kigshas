777e4389812d0dd271a779896bdd1670ad4ee251
magalutfullaev <mhoja9494@gmail.com>
2024-06-19 12:30:47 +0500

    init: Project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import MainPage from './pages/Main/MainPage.tsx'
+
+function App() {
+
+  return (
+    <MainPage />
+  )
+}
+
+export default App
---
+++ src/pages/Main/MainPage.tsx
@@ -0,0 +1,9 @@
+const MainPage = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default MainPage
