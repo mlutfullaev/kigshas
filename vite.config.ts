8949ae3d89e2dfd8098d1348c3d92a4d934a6b7e
magalutfullaev <mhoja9494@gmail.com>
2024-07-26 15:17:21 +0500

    fix: docker

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
-  // server: {
-  //   port: 3000
-  // }
+  server: {
+    host: true,
+  }
 })
